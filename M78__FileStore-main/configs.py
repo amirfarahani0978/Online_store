42b3a17663567d9af7ec9d36327eb5f8cae5f99d
amir <hassaniamirhossein0978@gmail.com>
2022-09-07 16:18:42 +0430

    add Online_store

---
+++ M78__FileStore-main/configs.py
@@ -0,0 +1,11 @@
+DB_CONNECTION = {
+    "DBNAME": "Store",
+    "USER": "postgres",
+    "PORT": 5432,
+    "PASSWORD": "amir1380"
+}
+
+INFO = {
+    "name": "Amirhossein File Store",
+    "description": "Back-end Developer",
+}
